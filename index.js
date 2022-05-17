a068c806b0a5607655a082bc440d4e927cd04205
dontroll17 <dontroll@yandex.ru>
2022-05-17 15:30:23 +0300

    feat: cors

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const bodyParser = require("body-parser");
 const authRouter = require('./src/routes/auth');
 const app = express();
 const PORT = process.env.PORT || 5555;
+const cors = require('cors');
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( {extended: true }));
+app.use(cors());
 app.use('/auth',authRouter);
 
 app.listen(PORT,  () => {
