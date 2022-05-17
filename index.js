01302c410f3c555cb7ed1c8e3b09d573cccf2cb2
dontroll17 <dontroll@yandex.ru>
2022-05-17 16:37:30 +0300

    feat: cors io

--- index.js
+++ index.js
@@ -4,12 +4,21 @@ const authRouter = require('./src/routes/auth');
 const app = express();
 const PORT = process.env.PORT || 5555;
 const cors = require('cors');
+
+//socket.io init
 const { createServer } = require("http");
 const { Server } = require('socket.io');
 const httpServer = createServer(app);
-const io = new Server(httpServer);
+const io = new Server(httpServer, {
+    allowRequest: (req, callback) => {
+        const noOriginHeader = req.headers.origin === undefined;
+        callback(null, noOriginHeader);
+    }
+});
 
-
+io.on('connection', () => {
+    console.log('Got connect');
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( {extended: true }));
