9f665074d398c3689017118a7a8a1d7a7738d431
Korvin <mr.gold1@mail.ru>
2022-05-18 17:21:12 +0300

    added socket connection authorization

--- index.js
+++ index.js
@@ -4,21 +4,36 @@ const authRouter = require('./src/routes/auth');
 const app = express();
 const PORT = process.env.PORT || 5555;
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 
 //socket.io init
 const { createServer } = require("http");
 const { Server } = require('socket.io');
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
-    allowEIO3: true,
     cors: {
         origin: "http://localhost:3000",
-        credentials: true
     }
 });
 
-io.on('connection', () => {
+// socket authorization verifier
+io.use(function(socket, next){
+    try{
+        if(jwt.verify(socket.handshake.query.token, process.env.JWT_KEY)){
+            next();
+        }else{
+            next(new Error('not authorized'));
+        }
+    } catch(e){
+        next(new Error("invalid"));
+    }
+})
+
+io.on('connection', (socket) => {
     console.log('Got connect');
+    socket.on('emit_method', (msg) => {
+        console.log('message: ' + msg);
+    });
 });
 
 app.use(bodyParser.json());
